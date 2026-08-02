6afd8910abdaf547e74fbe605d43bae54d8d9160
Anjali Nagar <apn.anjalinagar@gmail.com>
2026-08-02 22:42:42 +0530

    Initial LMS project

---
+++ client/src/context/auth-context/index.jsx
@@ -0,0 +1,27 @@
+import axiosInstance from "@/api/axiosInstance";
+import { initialSignInFormData, initialSignUpFormData } from "@/config";
+import { registerService } from "@/services";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+
+export default function AuthProvider({ children }) {
+  const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
+  const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+  async function handleRegisterUser(){
+    const data = await registerService(signUpFormData);
+    
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        signInFormData,
+        setSignInFormData,
+        signUpFormData,
+        setSignUpFormData,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ server/routes/auth-routes/index.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const {registerUser} = require('../../controllers/auth-controller/index')
+const router = express.Router();
+router.post('/register',registerUser);
